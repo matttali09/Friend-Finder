b58c0d9d4970647776c585ea3a5f2211e7cdfce8
matttali09 <matttaliancich09@gmail.com>
2018-12-18 20:36:45 -0500

    added a lot of styling and logic for compatibility score

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ var app = express();
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
