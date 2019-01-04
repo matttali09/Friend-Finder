8bf6f4b11a54b4fbfee56232040c7dc11c4c6619
matttali09 <matttaliancich09@gmail.com>
2019-01-04 10:58:23 -0500

    fixed survey logic

--- app/public/assets/js/survey.js
+++ app/public/assets/js/survey.js
@@ -1,49 +1,53 @@
 // create the submit fucntion that will grab the data and reset the survey
 $(".submit").on("click", function (event) {
     event.preventDefault();
-// add logic for required fields
-if ($("#friend-name").val() == null || $("#friend-age").val() == null || $("#friend-gender").val() == null || $("#friend-FB").val() == null) {
-    // create array for answersArray value can be stored
-    var array = [];
-    var i = 1;
-    while ($("#ans" + i + "").val() != null) {
-        array.push($("#ans" + i + "").val());
-        i++;
-    };
-    console.log(array);
+    console.log($("#friend-name").val());
+    console.log($("#friend-age").val());
+    console.log($("#friend-gender").val());
+    console.log($("#friend-FB").val());
+    // add logic for required fields
+    if ($("#friend-name").val() != null && $("#friend-age").val() != null && $("#friend-gender").val() != null && $("#friend-FB").val() != null) {
+        // create array for answersArray value can be stored
+        var array = [];
+        var i = 1;
+        while ($("#ans" + i + "").val() != null) {
+            array.push($("#ans" + i + "").val());
+            i++;
+        };
+        console.log(array);
 
 
-    // Here we grab the form elements
-    var newFriend = {
-        friendName: $("#friend-name").val().trim(),
-        friendAge: $("#friend-age").val().trim(),
-        friendGender: $("#friend-gender").val().trim(),
-        friendFB: $("#friend-FB").val().trim(),
-        answerArray: array
-    };
+        // Here we grab the form elements
+        var newFriend = {
+            friendName: $("#friend-name").val().trim(),
+            friendAge: $("#friend-age").val().trim(),
+            friendGender: $("#friend-gender").val().trim(),
+            friendFB: $("#friend-FB").val().trim(),
+            answerArray: array
+        };
 
-    console.log(newFriend);
+        console.log(newFriend);
 
 
-    $.post("/api/friends", newFriend,
-        function (data) {
+        $.post("/api/friends", newFriend,
+            function (data) {
 
-            alert("Thank You for Adding Your Friendly Face to Our Database :)!");
+                alert("Thank You for Adding Your Friendly Face to Our Database :)!");
 
-            // Clear the form when submitting
-            $("#friend-name").val("");
-            $("#friend-age").val("");
-            $("#friend-gender").val("");
-            // need to reset i back to 1 from previous time we used it
-            i = 1;
-            while ($("#ans" + i + "").val() != null) {
-                $("#ans" + i + "").val("1");
-                i++;
-            };
-            // do a redirect
-            res.redirect("/survey");
+                // Clear the form when submitting
+                $("#friend-name").val("");
+                $("#friend-age").val("");
+                $("#friend-gender").val("");
+                // need to reset i back to 1 from previous time we used it
+                i = 1;
+                while ($("#ans" + i + "").val() != null) {
+                    $("#ans" + i + "").val("1");
+                    i++;
+                };
+                // do a redirect
+                location.assign("/");
 
-        });
+            });
     }
     else {
         alert("Please Fill Out the Required Fields Friend! (*)")
